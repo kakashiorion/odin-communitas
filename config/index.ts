99259ec3782aa5e000b002b74a033172bceb168a
kakashiorion <nitin.kool.1808@gmail.com>
2023-06-20 00:45:31 +0530

    server changes

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,7 @@
-const dev = process.env.NODE_ENV !== "production";
+// const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://odin-communitas.vercel.app";
+// export const server = dev
+//   ? "http://localhost:3000"
+//   : "https://odin-communitas.vercel.app";
+  
+export const server = "http://localhost:3000"
