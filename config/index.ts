590651392f55cd5e22e4d30ec96a6e7b2a96bf38
kakashiorion <nitin.kool.1808@gmail.com>
2023-06-19 23:48:03 +0530

    Changed server link

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://odin-communitas-plkd7vsqx-kakashiorion.vercel.app";
+  : "https://odin-communitas.vercel.app";
